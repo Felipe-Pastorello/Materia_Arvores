63dee69f212190f3caf9fb6da2578f7c657e9b99
Felipe-Pastorello <felipe.pastorello@edu.unifil.br>
2025-06-05 22:02:41 -0300

    Atualizações rotações

--- src/ArvoreAVL.java
+++ src/ArvoreAVL.java
@@ -16,7 +16,7 @@ public class ArvoreAVL {
             return node;
         System.out.println(node);
         // Altura
-        node.altura = 1 + Math.max(altura(node.esquerda), altura(node.direita)); //A função Math.max serve para achar o maior valor entre duas variáveis
+       node.altura = 1 + Math.max(altura(node.esquerda), altura(node.direita)); //A função Math.max serve para achar o maior valor entre duas variáveis
 
         // Balanceamento
         int b = balanceamento(node); //Ver a diferença de altura entre a esquerda e direita
@@ -24,7 +24,8 @@ public class ArvoreAVL {
         // Rotações
 
         // Caso Esquerda-Esquerda (LL)
-
+        if (b > 1 && valor < node.esquerda.valor)
+            return rotacaoDireita(node);
 
         // Caso Direita-Direita (RR)
 
@@ -40,5 +41,31 @@ public class ArvoreAVL {
 
     private int balanceamento(NoAVL node) {
         return node == null ? 0 : altura(node.esquerda) - altura(node.direita);
+    }
+
+    private NoAVL rotacaoDireita(NoAVL y) {
+        NoAVL x = y.esquerda;
+        NoAVL T2 = x.direita;
+
+        x.direita = y;
+        y.esquerda = T2;
+
+        y.altura = 1 + Math.max(altura(y.esquerda), altura(y.direita));
+        x.altura = 1 + Math.max(altura(x.esquerda), altura(x.direita));
+
+        return x;
+    }
+
+    private NoAVL rotacaoEsquerda(NoAVL x) {
+        NoAVL y = x.direita;
+        NoAVL T2 = y.esquerda;
+
+        y.esquerda = x;
+        x.direita = T2;
+
+        x.altura = 1 + Math.max(altura(x.esquerda), altura(x.direita));
+        y.altura = 1 + Math.max(altura(y.esquerda), altura(y.direita));
+
+        return y;
     }
 }
